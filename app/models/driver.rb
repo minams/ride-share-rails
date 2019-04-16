56fbfd60fffec409be47b5aa05206d7995694a67
laneia <laneiamurray@gmail.com>
2019-04-16 15:37:44 -0700

    added skeleton for money_earned and average_rating driver methods

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -2,4 +2,10 @@ class Driver < ApplicationRecord
   has_many :trips, dependent: :destroy
   validates :name, presence: true
   validates :vin, presence: true
+
+  def money_earned
+  end
+
+  def average_rating
+  end
 end
