b1754da1c77bb23e686cffa734952e180a2b99d7
laneia <laneiamurray@gmail.com>
2019-04-16 16:11:50 -0700

    added driver average_rating method

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,7 +1,6 @@
 class Driver < ApplicationRecord
   has_many :trips, dependent: :destroy
-  validates :name, presence: true
-  validates :vin, presence: true
+  validates :name, :vin, presence: true
 
   def money_earned
     money = 0
@@ -14,5 +13,20 @@ class Driver < ApplicationRecord
   end
 
   def average_rating
+    rating = 0
+    no_rating = 0
+
+    trips.each do |trip|
+      if trip.rating == nil
+        no_rating += 1
+      else
+        rating += trip.rating
+      end
+    end
+
+    rated_trips = trips.count - no_rating
+
+    average = (rating / rated_trips).to_f
+    return average.round(1)
   end
 end
