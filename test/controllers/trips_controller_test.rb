3fcb27fdac7916a2ea01d6bb7575ae742e089722
laneia <laneiamurray@gmail.com>
2019-04-17 09:32:06 -0700

    added trips controller tests

--- test/controllers/trips_controller_test.rb
+++ test/controllers/trips_controller_test.rb
@@ -2,22 +2,27 @@ require "test_helper"
 
 describe TripsController do
   describe "show" do
-    # Your tests go here
+    get trips_show_url
+    must_respond_with :success
   end
 
   describe "edit" do
-    # Your tests go here
+    get trips_edit_url
+    must_respond_with :success
   end
 
   describe "update" do
-    # Your tests go here
+    get trips_update_url
+    must_respond_with :success
   end
 
   describe "create" do
-    # Your tests go here
+    get trips_create_url
+    must_respond_with :success
   end
 
   describe "destroy" do
-    # Your tests go here
+    get trips_destroy_url
+    must_respond_with :success
   end
 end
