d7b1226809bda2f22b51daec65876d32e95a06c6
laneia <laneiamurray@gmail.com>
2019-04-17 11:23:57 -0700

    working on adding routes, correcting paths

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,22 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "homepages#index"
 
+  get "/", to: "trips#index", as: "homepages"
+
+  get "/passengers", to: "passengers#index"
+
+  get "/passengers/new", to: "passengers#new", as: "new_passenger"
+
+  get "/passengers/:id/edit", to: "passengers#edit", as: "edit_passenger"
+
+  get "/passengers/:id", to: "passengers#show", as: "passenger"
+
+  patch "passengers/:id", to: "passengers#update"
+
+  post "passengers", to: "passengers#create"
+
+  delete "/passengers/destroy/:id", to: "passengers#destroy", as: "delete_passenger"
+
   get "/drivers", to: "drivers#index"
 
   get "/drivers/new", to: "drivers#new", as: "new_driver"
